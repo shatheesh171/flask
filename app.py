ed985f431797e2f3f5a0684bb46908266e69bdcf
shatheesh17 <36799558+shatheesh17@users.noreply.github.com>
2021-07-29 08:24:09 +0530

    Change database to postgres

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from db import db
 from flask import Flask
 from flask_restful import Api
@@ -11,7 +12,7 @@ app=Flask(__name__)
 api=Api(app)
 app.secret_key = 'goyala'
 
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL','sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 
 @app.before_first_request
